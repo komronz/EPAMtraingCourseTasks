34a1728b439e94dbe0fc8a51806074501880a77e
komronz <pandacoder0820@gmail.com>
2022-07-27 19:37:55 +0500

    Initial commit

---
+++ main/java/Training01.java
@@ -0,0 +1,31 @@
+//todo Java 5
+//*Generics
+//*Annotations
+//*Autoboxing/unboxing
+//*Enumerations
+//*Varargs
+//*Enhanced for each loop
+//*Static imports
+//*New concurrency utilities in java.util.concurrent
+//*Scanner class for parsing data from various input streams and buffers
+//todo Java 8
+//*Lambada
+//*Stream Api
+//*Functional interface and default methods
+//*Optionals
+//*Annotation on Java Types
+//*Unsigned Integer Arithmetic
+//*Repeating annotations
+//*New Date and Time Api
+//*Remove the permanent generation from GC
+//todo Java 9
+//*Java platform module system
+//*Interface Private Methods
+//*HTTP 2 Client
+//*JShell - REPL Tool
+//*Platform and JVM logging
+class Training01 {
+    public static void main(String[] args) {
+
+    }
+}
